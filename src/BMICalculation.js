04fd4cf2f31594375ca9c47c24652859f9dd65f6
Chaste20 <chastesauveur@gmail.com>
2023-12-14 01:40:58 -0800

    Unit testing of BMICalculation with jest framework and validation for null values as well as negative values

--- src/BMICalculation.js
+++ src/BMICalculation.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const BMICalculation = () => {
+/*const BMICalculation = () => {
   const [weight, setWeight] = useState('');
   const [height, setHeight] = useState('');
   const [bmi, setBMI] = useState(null);
@@ -41,6 +41,77 @@ const BMICalculation = () => {
       <br />
       <button onClick={calculateBMI}>Calculate BMI</button>
       {bmi && <p>BMI: {bmi}</p>}
+    </div>
+  );
+};*/
+const BMICalculation = () => {
+  const [weight, setWeight] = useState('');
+  const [height, setHeight] = useState('');
+  const [bmi, setBMI] = useState(null);
+  const [error, setError] = useState('');
+
+  const calculateBMI = () => {
+    if (!weight || !height) {
+      setError('Please enter both weight and height.');
+      setBMI(null);
+    } else {
+      const weightValue = parseFloat(weight);
+      const heightValue = parseFloat(height);
+
+      if (
+        !isNaN(weightValue) &&
+        !isNaN(heightValue) &&
+        weightValue > 0 &&
+        heightValue > 0 &&
+        weightValue <= 300 &&
+        heightValue <= 3
+      ) {
+        const bmiValue = (weightValue / Math.pow(heightValue, 2)).toFixed(1);
+        setBMI(bmiValue);
+        setError('');
+      } else {
+        setError('Please enter valid values for weight(max 3 meters) and height(300 kg).');
+        setBMI(null);
+      }
+    }
+  };
+
+  return (
+    <div>
+      <h2>BMI Calculation</h2>
+      <label>
+        Weight (kg):
+        <input
+          type="number"
+          step="0.1"
+          min="1.0"
+          max="300.0"
+          value={weight}
+          onChange={(e) => {
+            const newValue = e.target.value;
+            setWeight(newValue > 0 ? newValue : '');
+          }}
+        />
+      </label>
+      <br />
+      <label>
+        Height (m):
+        <input
+          type="number"
+          step="0.1"
+          min="0.1"
+          max="3.0"
+          value={height}
+          onChange={(e) => {
+            const newValue = e.target.value;
+            setHeight(newValue > 0 ? newValue : '');
+          }}
+        />
+      </label>
+      <br />
+      <button onClick={calculateBMI}>Calculate BMI</button>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {bmi !== null && !error && <p>BMI: {bmi}</p>}
     </div>
   );
 };
---
+++ src/BMICalculation.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import BMICalculation from './BMICalculation';
+
+test('calculates BMI correctly with valid inputs', () => {
+  const { getByLabelText, getByText } = render(<BMICalculation />);
+
+  // Mock valid inputs
+  fireEvent.change(getByLabelText(/Weight \(kg\)/i), { target: { value: '70' } });
+  fireEvent.change(getByLabelText(/Height \(m\)/i), { target: { value: '1.75' } });
+
+  // Trigger BMI calculation
+  fireEvent.click(getByText(/Calculate BMI/i));
+
+  // Check if BMI is displayed correctly
+  expect(getByText(/BMI: 22.9/i)).toBeInTheDocument();
+});
+
+test('displays error with empty inputs', () => {
+  const { getByText } = render(<BMICalculation />);
+
+  // Trigger BMI calculation with empty inputs
+  fireEvent.click(getByText(/Calculate BMI/i));
+
+  // Check if error message is displayed
+  expect(getByText(/Please enter both weight and height/i)).toBeInTheDocument();
+});
+
+test('displays error with invalid inputs', () => {
+  const { getByLabelText, getByText } = render(<BMICalculation />);
+
+  // Mock invalid inputs (negative weight)
+  fireEvent.change(getByLabelText(/Weight \(kg\)/i), { target: { value: '-70' } });
+  fireEvent.change(getByLabelText(/Height \(m\)/i), { target: { value: '1.75' } });
+
+  // Trigger BMI calculation
+  fireEvent.click(getByText(/Calculate BMI/i));
+
+  // Check if error message is displayed
+  expect(getByText(/Please enter valid values for weight and height/i)).toBeInTheDocument();
+});
