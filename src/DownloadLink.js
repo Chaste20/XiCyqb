b7417fd3b964b9a39c2d5a449a3f299e6fc5afd6
Chaste20 <chastesauveur@gmail.com>
2023-12-26 05:45:12 -0800

    Fixing broken dowload link

--- src/DownloadLink.js
+++ src/DownloadLink.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
 const DownloadLink = () => {
+
+  const onButtonClick = () => {
+    const pdfUrl = "Duck cree.pdf";
+    const link = document.createElement("a");
+    link.href = pdfUrl;
+    link.download = "Duck cree.pdf"; // specify the filename
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
+
   return (
     <div>
       <h2> Download Link to Resume</h2>
       {/* Add your resume download link here */}
-      <a href="XiCyqb\xicyqb\public\Innovance.pdf" download>
-        Download Resume
-      </a>
+      <button onClick={onButtonClick}>
+                  <a href="#">Download PDF</a>  
+                </button>
     </div>
   );
 };
